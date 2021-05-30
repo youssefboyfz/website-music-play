2fc35c94b49994107f35650646599ca6e3e52cfc
youssefboyfz <85025149+youssefboyfz@users.noreply.github.com>
2021-05-30 18:54:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+ var mysong =document.getElementById("mysong");
+var icon =document.getElementById("icon");
+icon.onclick =function(){
+    if(mysong.paused){
+        mysong.play();
+        icon.src ="media/pause.png";
+    }
+    else{
+        mysong.pause();
+        icon.src ="media/play.png";
+
+    }
+}
+      
